cd65a0b4364474519999d170f6a57503a7d887f7
Mamadou Fam <fammamadou30@gmail.com>
2021-07-01 23:51:11 +0000

    Voila le deuxieme commit du projet

--- cercle.py
+++ cercle.py
@@ -1,28 +1,37 @@
 from typing import DefaultDict
 import math
 
-
-
+#PARTIE A
+#Création de la classe Point
 class Point:
+    #Constructeur de la classe Point
     def __init__(self,x,y):
         self.x=x
         self.y=y
     @property
+    #Methode Afficher() de la classe Point
     def Afficher (self):
         print('POINT(%d,%d)' % (self.x,self.y))
 
+#Création de la classe Cercle
 class Cercle(Point):
-    def __init__(self,centre=None,rayon=None):
+    def __init__(self,centre=Point,rayon=float):
         super().__init__()
         self.centre=centre
         self.rayon=rayon
+     #Création des Methodes de la classe Cercle
     @property
     def getPerimetre (self):
         return (2*math.pi*self.rayon)
     def getSurface(self):
         return (math.pi*self.rayon**2)
+    def Afficher(self):
+        print('CERCLE(%d,%d,%d)' % (self.centre.x,self.centre.y,self.rayon))
     
-    
+#PARTIE B
+#Création de la classe Cylindre
+class Cylindre(Cercle):
+
 
 
   
