f4f2421d420037edbe822c181641109ec481e360
Mamadou Fam <fammamadou30@gmail.com>
2021-07-02 01:04:45 +0000

    troisieme commit du projet

--- cercle.py
+++ cercle.py
@@ -19,19 +19,30 @@ class Cercle(Point):
         super().__init__()
         self.centre=centre
         self.rayon=rayon
-     #Création des Methodes de la classe Cercle
+    #Création des Methodes de la classe Cercle
     @property
     def getPerimetre (self):
         return (2*math.pi*self.rayon)
     def getSurface(self):
-        return (math.pi*self.rayon**2)
+        return (math.pi*(self.rayon**2))
     def Afficher(self):
         print('CERCLE(%d,%d,%d)' % (self.centre.x,self.centre.y,self.rayon))
     
 #PARTIE B
 #Création de la classe Cylindre
 class Cylindre(Cercle):
+    def __init__(self,cercleA=Cercle,hauteur=float):
+        super().__init__()
+        self.cercleA=cercleA
+        self.hauteur=hauteur
+    #Création methode getVolume() permettant de retourner le volume du cylindre
+        def getVolume(self):
+            return math.pi*(self.rayon**2)*self.hauteur
 
+A=Point(2,4)
+A.Afficher
+cercleDeCentreA=Cercle(A,5)
+cercleDeCentreA.Afficher
 
 
   
